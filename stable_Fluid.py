2934a0c757f5d7ac7a4fe7f68228cc5790452e11
GameTithe <wkdwonum@gmail.com>
2025-03-23 23:52:32 +0900

    백업

--- stable_Fluid.py
+++ stable_Fluid.py
@@ -7,20 +7,24 @@ NDIM = 2
 
 N = ti.Vector([128, 128])
 #grid = ti.Vector.field(NDIM, dtype = ti.i32, shape = (N[0], N[1]))
-divergence = ti.field(dtype = ti.f32, shape = (N[0], N[1]))
+divergence = ti.field(dtype = ti.f32, shape = (N[0] + 2, N[1] + 2))
 
-P = ti.field(dtype = ti.f32, shape = (N[0], N[1]))
+P = ti.field(dtype = ti.f32, shape = (N[0] + 2, N[1] + 2))
+
+#origin
 O = ti.Vector.field(NDIM, dtype = ti.f32, shape=(1, ))
+#length of grid
 L = ti.Vector.field(NDIM, dtype = ti.f32, shape=(1, ))
+#length of cell 
 D = ti.Vector.field(NDIM, dtype = ti.f32, shape=(1,))
 
 #velocity  field
-U0 = ti.Vector.field(NDIM, dtype = ti.f32, shape = (N[0], N[1]))
-U1 = ti.Vector.field(NDIM, dtype = ti.f32, shape = (N[0], N[1]))
+U0 = ti.Vector.field(NDIM, dtype = ti.f32, shape = (N[0] + 2, N[1] + 2))
+U1 = ti.Vector.field(NDIM, dtype = ti.f32, shape = (N[0] + 2, N[1] + 2))
 
 #density field
-D0 = ti.Vector.field(NDIM, dtype = ti.f32, shape = (N[0], N[1]))
-D1 = ti.Vector.field(NDIM, dtype = ti.f32, shape = (N[0], N[1]))
+D0 = ti.Vector.field(NDIM, dtype = ti.f32, shape = (N[0] + 2, N[1] + 2))
+D1 = ti.Vector.field(NDIM, dtype = ti.f32, shape = (N[0] + 2, N[1] + 2))
 
 
 #source field
@@ -32,22 +36,26 @@ grav2D = ti.Vector([0.0 , -9.8])
 grav3D = ti.Vector([0.0 , -9.8, 0.0])
 
 #diffusion const 
-kd = 0.5
+kd = 0.001
 
 #source const 
-aS = 0.5
+aS = 0.001
 
-dt = 1e-1
+dt = 1e-3
 
 @ti.kernel
 def initialize_2D(): 
-    # for i, j in ti.ndrange(N[0], N[1]):
-    #     grid[i, j] = ti.Vector([i, j]) 
-               
+    #Init Grid         
     O[0] = ti.Vector([0.0, 0.0])
-    L[0] = ti.Vector([1.0, 1.0])
+    L[0] = ti.Vector([1.0, 1.0]) 
     D[0] = L[0] / N[0] 
-     
+    
+    #Init Density, Velocity
+    for i,j in ti.ndrange((1, N[0] + 1), (1,N[1] + 1)):
+        D0[i,j] = ti.Vector([0, 0])
+        D1[i,j] = ti.Vector([0, 0])
+        U0[i,j] = ti.Vector([0, 0])
+        U1[i,j] = ti.Vector([0, 0])  
      
 def SwapU():
     global U0, U1
@@ -61,39 +69,89 @@ def SwapD():
     global D0, D1
     D0, D1 = D1, D0 
     
+#@ti.kernel
 @ti.func
-def AddSource():
-    for i, j in ti.ndrange(N[0], N[1]):
-        D1[i,j] += S0[0] * dt
- 
+def AddSource_D(mouse_pos: ti.types.vector(2, ti.f32)):
+   
+    index = ti.cast(mouse_pos * N, ti.i32) 
+    
+    minX = max(index.x - 3, 1)
+    minY = max(index.y - 3, 1)
+    
+    maxX = min(index.x + 3, N[0])
+    maxY = min(index.y + 3, N[0])
+    
+        
+    for i, j in ti.ndrange( (minX, maxX + 1 ), (minY, maxY + 1)):
+        D1[i, j] += ti.Vector([15.0, 0.0])  
+        
+@ti.func
+def AddSource_U(mouse_pos: ti.types.vector(2, ti.f32)):
+   
+    index = ti.cast(mouse_pos * N, ti.i32) 
+    
+    minX = max(index.x - 3, 1)
+    minY = max(index.y - 3, 1)
+    
+    maxX = min(index.x + 3, N[0])
+    maxY = min(index.y + 3, N[0])
+    
+        
+    for i, j in ti.ndrange( (minX, maxX + 1 ), (minY, maxY + 1)): 
+        U1[i, j] += ti.Vector([0.0, 10.0]) 
+        
+@ti.func
+def set_bnd(b: ti.i32, x: ti.template()):
+    
+    for i in ti.ndrange(N[0] + 2):
+        x[i, 0] = x[i, 1] if b == 2 else -x[i, 1]  # y=0 경계
+        x[i, N[1] + 1] = x[i, N[1]] if b == 2 else -x[i, N[1]]  # y=N+1 경계
+    
+    for j in ti.ndrange(N[1] + 2):
+        x[0, j] = x[1, j] if b == 1 else -x[1, j]  # x=0 경계
+        x[N[0] + 1, j] = x[N[0], j] if b == 1 else -x[N[0], j]  # x=N+1 경계
+    
+    
+    x[0, 0] = 0.5 * (x[1, 0] + x[0, 1])
+    x[0, N[1] + 1] = 0.5 * (x[1, N[1] + 1] + x[0, N[1]])
+    x[N[0] + 1, 0] = 0.5 * (x[N[0], 0] + x[N[0] + 1, 1])
+    x[N[0] + 1, N[1] + 1] = 0.5 * (x[N[0], N[1] + 1] + x[N[0] + 1, N[1]])
+
 @ti.func
 def Project():
-    h  = 1.0 / float(N[0])
+    h = 1.0 / N[0]
     
-    for i, j in ti.ndrange(1, int(N[0]) - 1, 1 , int( N[1]) - 1) :  
-        divergence[i, j] = -0.5 * h * ((U1[i+1, j].x - U1[i-1, j].x) + (U1[i, j+1].y - U1[i, j-1].y))
-
-        P[i , j] = 0
-        
-    # boundary
+    for i, j in ti.ndrange((1, N[0] + 1), (1, N[1] + 1)):  
+        divergence[i, j] = -0.5 * h * ((U0[i+1, j].x - U0[i-1, j].x) + (U0[i, j+1].y - U0[i, j-1].y))
+        P[i, j] = 0.0
+    set_bnd(0, divergence)
+    set_bnd(0, P)
     
-    for k in ti.static(20):
-        for i, j in ti.ndrange(1 , int(N[0]) - 1, 1, int(N[1]) - 1 ):
+    for k in ti.static(range(20)):
+        for i, j in ti.ndrange((1, N[0] + 1), (1, N[1] + 1)):
             P[i, j] = (divergence[i, j] + P[i-1, j] + P[i+1, j] + P[i, j-1] + P[i, j+1]) / 4.0
-
-    for i, j in ti.ndrange(1, int(N[0]) - 1, 1, int(N[1]) - 1):
-        # x방향 속도 보정: 중앙 차분으로 압력의 기울기를 계산
-        U0[i, j].x -= 0.5 * (P[i+1, j] - P[i-1, j]) / h
-        # y방향 속도 보정: 중앙 차분으로 압력의 기울기를 계산
-        U0[i, j].y -= 0.5 * (P[i, j+1] - P[i, j-1]) / h 
-      
+    set_bnd(0, P)
+    
+    for i, j in ti.ndrange((1, N[0] + 1), (1, N[1] + 1)): 
+        U1[i, j].x -= 0.5 * (P[i+1, j] - P[i-1, j]) / h 
+        U1[i, j].y -= 0.5 * (P[i, j+1] - P[i, j-1]) / h  
+    set_bnd(1, U1)  # x 방향 속도
+    set_bnd(2, U1)  # y 방향 속도
+ 
 @ti.func
 def Advect():
-    for i, j in ti.ndrange(N[0], N[1]):
-        pos = ti.Vector([i - U0[i,j].x * dt, j - U0[i,j].y * dt])
+    
+    dt0 = dt * N[0]
+    for i, j in ti.ndrange( ( 1, N[0]), (1 , N[1])):
+        
+        pos = ti.Vector([i - U0[i,j].x * dt0, j - U0[i,j].y * dt0]) 
+        
+        pos.x = ti.min(ti.max(pos.x, 0.5), N[0] + 0.5)
+        pos.y = ti.min(ti.max(pos.y, 0.5), N[1] + 0.5)
+        
         #boundary check
-        i0 = ti.cast(i, ti.i32)
-        j0 = ti.cast(j, ti.i32)
+        i0 = ti.cast(pos.x, ti.i32)
+        j0 = ti.cast(pos.y, ti.i32) 
         i1 = i0 + 1
         j1 = j0 + 1
 
@@ -104,7 +162,7 @@ def Advect():
         t0 = 1 - t1
           
         D1[i, j] = s0 * (t0 * D0[i0, j0] + t1 * D0[i0, j1]) + s1 * (t0 * D0[i1, j0] + t1 * D0[i1, j1])
-
+    set_bnd(0, D1)
 
 @ti.func
 def Diffuse():
@@ -112,40 +170,52 @@ def Diffuse():
     # boundary check
     
     for k in ti.static(range(20)) :
-        for i, j in ti.ndrange(N[0], N[1]):
-            D1[i, j] = (D0[i ,j] + kd * ( D1[i - 1, j] + D1[i + 1, j] + D1[i,j -1] + D1[i, j  + 1])) / (1 + 4 * kd)
- 
+        for i, j in ti.ndrange( ( 1, N[0] + 1), ( 1, N[1] + 1) ):
+            D1[i, j] = (D0[i ,j] + kd * ( D0[i - 1, j] + D0[i + 1, j] + D0[i,j -1] + D0[i, j  + 1])) / (1 + 4 * kd)
+            #D1[i, j] = (D0[i ,j] + kd * ( D1[i - 1, j] + D1[i + 1, j] + D1[i,j -1] + D1[i, j  + 1])) / (1 + 4 * kd)
+
+        set_bnd(0, D1)
     # boundary check
     
 @ti.kernel
-def dens_step():
-    AddSource()
-    SwapD()
+def dens_step(mouse_pos: ti.types.vector(2, ti.f32), add_force: ti.i32):
     
+    if add_force:
+        AddSource_D(mouse_pos)
+    
+    SwapD()
     Diffuse()
-    SwapD()
     
+    SwapD()
     Advect()
     
 @ti.kernel
-def vel_step():
+def vel_step(mouse_pos: ti.types.vector(2, ti.f32), add_force: ti.i32):
     
-    AddSource()
+    if add_force: 
+        AddSource_U(mouse_pos)
+    
     SwapU()
-    
+    Diffuse()
+
     Project()
     SwapU()
     
     Advect()
     Project() 
 
+pixels = ti.field(dtype=ti.f32, shape=(N[0] + 2, N[1] + 2, 3))
+@ti.kernel
+def update_pixcels():
+   # a = 1
+    for i, j in ti.ndrange( (1, N[0] + 1), ( 1, N[1] + 1) ):
+        density = D0[i ,j][0] 
+        pixels[i,j,0] = ti.min(1.0, density)  # R
+        pixels[i,j,1] = ti.min(1.0, density * 0.5)  # G
+        pixels[i,j,2] = ti.min(1.0, density * 0.2)  # B
     
-def step(): 
-    dens_step()
-    vel_step()
     
-    
-window = ti.ui.Window("Taichi Cloth Simulation on GGUI", (1024, 1024),
+window = ti.ui.Window("TaichiS Stable Fluid", (1024, 1024),
                       vsync=True)
 canvas = window.get_canvas()
 canvas.set_background_color((1, 1, 1))
@@ -154,10 +224,34 @@ camera = ti.ui.Camera()
 
 current_t = 0.0 
 
+mouse_pos = ti.Vector([0.5, 0.5])
+add_force = False
+
 initialize_2D()
+
 while window.running:
-    vel_step()
-    dens_step()
-
-
-    step() 
+    # Get mouse position 
+    # for e in window.get_events():
+    #     if window.is_pressed(ti.ui.LMB):  # 마우스 왼쪽 버튼 눌렸을 때
+    #         mx, my = window.get_cursor_pos()
+    #         mouse_pos = ti.Vector([mx, my])
+    #         add_force = True  
+    #     elif not window.is_pressed(ti.ui.LMB):  # 마우스 왼쪽 버튼 떼었을 때
+    #         add_force = False 
+            
+    add_force = window.is_pressed(ti.ui.LMB)
+    if add_force:
+        mx, my = window.get_cursor_pos()
+        mouse_pos = ti.Vector([mx, my])
+        
+    dens_step(mouse_pos, int(add_force))    
+    vel_step(mouse_pos, int(add_force))
+    
+    update_pixcels()
+    canvas.set_image(pixels)
+    
+    window.show()
+    
+    add_force = False
+    
+    
